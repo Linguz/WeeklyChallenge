691b4d98b028337dcc92efd50f57d81a5ff462b9
Carlos A Martinez <Linguz2@gmail.com>
2021-12-02 11:41:33 -0500

    First commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeeklyChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
