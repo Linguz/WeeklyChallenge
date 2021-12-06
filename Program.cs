a2bb6ae9c7c8e3bcbd45d789264e9f9ae1746f83
Carlos A Martinez <Linguz2@gmail.com>
2021-12-06 18:26:49 -0500

    Added OverTime and UniqueFract and a basic menu system.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WeeklyChallenge
 {
@@ -6,8 +7,45 @@ namespace WeeklyChallenge
     {
         static void Main(string[] args)
         {
-            while (true)
-                RockPaperScissors();
+            Boolean exit = false;
+            while (!exit)
+            {
+                Console.WriteLine();
+                List<string> options = new List<string>() { "q", "1", "2", "3" };
+                Console.WriteLine("1: Rock Paper Scissors");
+                Console.WriteLine("2: OverTime");
+                Console.WriteLine("3: UniqueFract");
+                Console.WriteLine("q: Exit");
+                
+                Console.Write("Select a choice: ");
+                string input = Console.ReadLine();
+                
+                while (!(options.Contains(input)))
+                {
+                    Console.Write("Please select a valid choice: ");
+                    input = Console.ReadLine();
+                }
+
+                switch(input)
+                {
+                    case "q":
+                        exit = true;
+                        break;
+                    case "1":
+                        RockPaperScissors();
+                        break;
+                    case "2":
+                        OverTime();
+                        break;
+                    case "3":
+                        UniqueFract();
+                        break;
+                    default:
+                        Console.WriteLine("You're not meant to get here.");
+                        exit = true;
+                        break;
+                }
+            }
         }
 
         private static void RockPaperScissors()
@@ -23,7 +61,6 @@ namespace WeeklyChallenge
                 {
                     cont = false;
                 }
-                Console.WriteLine("");
             }
             
             cont = true;
@@ -35,7 +72,6 @@ namespace WeeklyChallenge
                 {
                     cont = false;
                 }
-                Console.WriteLine("");
             }
 
             Console.WriteLine(RockPaperScissorsLogic(c1, c2));
@@ -85,6 +121,86 @@ namespace WeeklyChallenge
             }
 
             return "CONFUSION";
+        }
+
+        private static void OverTime()
+        {
+            double[] parameters = new double[4];
+            string[] queries = new string[] {"Start time: ", "End time: ", "Hourly rate: ", "Overtime multiplier: "};
+
+            for(int i = 0; i < 4; i++)
+            {
+                double temp;
+                bool cont = true;
+                while (cont)
+                {
+                    Console.Write(queries[i]);
+                    try
+                    {
+                        temp = Double.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please input a number.");
+                        continue;
+                    }
+                    
+                    parameters[i] = temp;
+                    cont = false;
+                }
+            }
+            OverTimePay(parameters[0], parameters[1], parameters[2], parameters[3]);
+        }
+        private static void OverTimePay(double start, double end, double rate, double multiplier)
+        {
+            // 9-17 is regular hours
+            // after 17 is overtime
+            double total = 0;
+            if (end <= 17)
+            {
+                // no overtime
+                total = (end-start) * rate;
+            }
+            else
+            {
+                double baseTotal = (17-start) * rate;
+                double overTotal = (end-17) * rate * multiplier;
+                total = baseTotal + overTotal;
+            }
+
+            if (start < 9)
+            {
+                Console.WriteLine("You should not begin work before 9am. Please check with your manager to ensure proper compensation.");
+            }
+
+            Console.WriteLine($"{total:C}");
+        }
+    
+        private static void UniqueFract()
+        {
+            double total = 0;
+            List<double> holder = new List<double>();
+            for (double d = 1; d < 10; d++)
+            {
+                for (double n = 1; n < 9; n++)
+                {
+                    double fraction = n/d;
+                    if (fraction >= 1)
+                    {
+                        break;
+                    }
+                    if (holder.Contains(fraction))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        holder.Add(fraction);
+                        total += fraction;
+                    }
+                }
+            }
+            Console.WriteLine($"Total of all unique fractions whose numerator and denominator are less than 1: {total}");
         }
     }
 }
